8604532ecf5e2c81e40bd96f8035b3c23de3bf15
Petro Sokolnykov <you@example.com>
2017-11-25 23:08:39 +0200

    do not conver any symbols for turboContent

--- src/sokolnikov911/YandexTurboPages/SimpleXMLElement.php
+++ src/sokolnikov911/YandexTurboPages/SimpleXMLElement.php
@@ -16,38 +16,12 @@ class SimpleXMLElement extends SimpleXMLE
      * @param string $namespace
      * @return SimpleXMLE
      */
-    public function addChild($name, $value = null, $namespace = null): SimpleXMLE
-    {
-        if ($value !== null and is_string($value) === true) {
-            $value = $this->convertSymbols($value);
-        }
-
-        return parent::addChild($name, $value, $namespace);
-    }
-
-    /**
-     * @param string $name
-     * @param string $value
-     * @param string $namespace
-     * @return SimpleXMLE
-     */
     public function addCdataChild($name, $value = null, $namespace = null): SimpleXMLE
     {
-        if ($value !== null and is_string($value) === true) {
-            $value = $this->convertSymbols($value);
-        }
-
         $element = $this->addChild($name, null, $namespace);
         $dom = dom_import_simplexml($element);
         $elementOwner = $dom->ownerDocument;
         $dom->appendChild($elementOwner->createCDATASection($value));
         return $element;
-    }
-    
-    private function convertSymbols(string $content): string
-    {
-        $content = str_replace('&',  '&amp;',  $content);
-        
-        return $content;
     }
 }
