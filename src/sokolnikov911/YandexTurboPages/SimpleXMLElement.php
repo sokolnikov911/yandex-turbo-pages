af198546652e2c1d2f73d6123344147d18a64b97
Petro Sokolnykov <you@example.com>
2017-11-25 20:30:19 +0200

    do not convert < and > symbols

--- src/sokolnikov911/YandexTurboPages/SimpleXMLElement.php
+++ src/sokolnikov911/YandexTurboPages/SimpleXMLElement.php
@@ -19,11 +19,7 @@ class SimpleXMLElement extends SimpleXMLE
     public function addChild($name, $value = null, $namespace = null): SimpleXMLE
     {
         if ($value !== null and is_string($value) === true) {
-            $value = str_replace('&',  '&amp;',  $value);
-            $value = str_replace('>',  '&gt;',   $value);
-            $value = str_replace('<',  '&lt;',   $value);
-            $value = str_replace('"',  '&quot;', $value);
-            $value = str_replace('\'', '&apos;', $value);
+            $value = $this->convertSymbols($value);
         }
 
         return parent::addChild($name, $value, $namespace);
@@ -38,11 +34,7 @@ class SimpleXMLElement extends SimpleXMLE
     public function addCdataChild($name, $value = null, $namespace = null): SimpleXMLE
     {
         if ($value !== null and is_string($value) === true) {
-            $value = str_replace('&',  '&amp;',  $value);
-            $value = str_replace('>',  '&gt;',   $value);
-            $value = str_replace('<',  '&lt;',   $value);
-            $value = str_replace('"',  '&quot;', $value);
-            $value = str_replace('\'', '&apos;', $value);
+            $value = $this->convertSymbols($value);
         }
 
         $element = $this->addChild($name, null, $namespace);
@@ -50,5 +42,14 @@ class SimpleXMLElement extends SimpleXMLE
         $elementOwner = $dom->ownerDocument;
         $dom->appendChild($elementOwner->createCDATASection($value));
         return $element;
+    }
+    
+    private function convertSymbols(string $content): string
+    {
+        $content = str_replace('&',  '&amp;',  $content);
+        $content = str_replace('"',  '&quot;', $content);
+        $content = str_replace('\'', '&apos;', $content);
+        
+        return $content;
     }
 }
